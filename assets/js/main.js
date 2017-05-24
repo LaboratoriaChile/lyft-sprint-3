ed79deba78cd1535ccab747cc6948afc62809867
cstapiarubio <cstapiarubio@gmail.com>
2017-05-24 00:04:58 -0400

    intentando hacer el js

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+
+
+var button=document.getElementById("boton-juego");
+
+button.addEventListener("click", function() {
+  var div = document.getElementById("contenedor-tablero");
+    if (div.style.display !== "none") {
+        div.style.display == "none";
+    }
+    else {
+        div.style.display == "block";
+    }
+
+});
+
+
+//Defino el tablero
+var tablero = [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0]
+]
+//Ingreso el auto en la posicion 3,5
+tablero[3][5] = "A";
+
+//Obtengo el div en donde se dibujará el tablero
+var divTablero = document.getElementById("tablero");
+
+//Variables temporales
+var fila, casilla;
+
+var x = document.getElementById("coorx").value;
+var y = document.getElementById("coory").value;
+
+//Recorro el arreglo para dibujar el tablero
+for(var i = 0; i<tablero.length; i++){
+  fila = document.createElement("div");
+  fila.classList.add("fila");
+  for(var j = 0; j<tablero[i].length; j++){
+    casilla = document.createElement("div");
+    casilla.innerHTML = tablero[i][j];
+    casilla.classList.add("casilla")
+    fila.appendChild(casilla);
+  }
+  divTablero.appendChild(fila);
+}
